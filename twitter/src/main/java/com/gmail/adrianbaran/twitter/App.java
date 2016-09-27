4c19551d64b7833e5949b647dc2d839f95fd43c5
Adrian Baran <adrianbaran@gmail.com>
2016-09-27 23:04:46 +0100

    pobieranie postów z API bezpośrednio do obiektów w javie

--- twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
+++ twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
@@ -1,11 +1,27 @@
 package com.gmail.adrianbaran.twitter;
 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 public class App {
+
+	public static final String API_ENDPOINT = "http://www.mocky.io/v2/57eadc901300005f2263dd81";
+
 	public static void main(String[] args) {
-			
+
 		RestTemplate restTemplate = new RestTemplate();
-	
+		
+		ResponseEntity<Post[]> httpEntity = restTemplate.getForEntity(
+				API_ENDPOINT, Post[].class);
+		Post[] resp = httpEntity.getBody();
+
+		
+		for (Post post : resp) {
+			System.out.println(post.getTitle());
+		}
 	}
 }
