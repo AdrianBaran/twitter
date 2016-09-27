dfe8ccb253565e56ccb2a7c76a6fee11f964a5c0
Adrian Baran <adrianbaran@gmail.com>
2016-09-27 20:51:40 +0100

    RestTemplate

--- twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
+++ twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
@@ -1,13 +1,11 @@
 package com.gmail.adrianbaran.twitter;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.web.client.RestTemplate;
+
+public class App {
+	public static void main(String[] args) {
+			
+		RestTemplate restTemplate = new RestTemplate();
+	
+	}
 }
