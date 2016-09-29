9d6a8ee17924b000f99cc13a81aff433a2ca5c59
Adrian Baran <adrianbaran@gmail.com>
2016-09-29 18:17:47 +0100

    pobranie z twitter homepaga na list username + msgtxt

--- twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
+++ twitter/src/main/java/com/gmail/adrianbaran/twitter/App.java
@@ -1,6 +1,5 @@
 package com.gmail.adrianbaran.twitter;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,22 +9,49 @@ import org.springframework.web.client.RestTemplate;
 
 import com.gmail.adrianbaran.twitter.trends.TrendWrapper;
 
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
 public class App {
 
-	public static final String API_ENDPOINT = "http://www.mocky.io/v2/57eadc901300005f2263dd81"; //LINK MOŻE WYGASNĄĆ
+	// public static final String API_ENDPOINT =
+	// "http://www.mocky.io/v2/57eadc901300005f2263dd81"; //LINK MOŻE WYGASNĄĆ
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TwitterException {
 
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Post[]> httpEntity = restTemplate.getForEntity(API_ENDPOINT, Post[].class);
-		Post[] resp = httpEntity.getBody();
+		ConfigurationBuilder cb = new ConfigurationBuilder();
 		
-		List<Post> postsList = new ArrayList<Post>(Arrays.asList(resp));
-		postsList.add(new Post(100, 10, "Adrian", "Baran"));
+		cb.setDebugEnabled(true)
+			.setOAuthConsumerKey("lt0OUdtVvC4RPBs71Vh5KD9Nu")
+			.setOAuthConsumerSecret("DuX5IPoYGtGYMhqiO4TFohREnveNkaMWMSMTjRlXrCk5qztdtl")
+			.setOAuthAccessToken("2436683348-mkq2pRWyRPw6l31YehZlBVK8mCQ6IWnSm5Lg3Lk")
+			.setOAuthAccessTokenSecret("NantOnZFBXe6YNEoXw3bJ1kcDEoY5jPQoGBt1ZOe5AOjU");
 		
-		for (Post post : postsList) {
-			System.out.println(post.getTitle());
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		twitter4j.Twitter twitter = tf.getInstance();
+		
+		List<Status> status = twitter.getHomeTimeline();
+		
+		for (Status status2 : status) {
+			
+			System.out.println(status2.getUser().getName()+ " ---- " + status2.getText());
 		}
+		
+		
+		
+//		RestTemplate restTemplate = new RestTemplate();
+//		ResponseEntity<Post[]> httpEntity = restTemplate.getForEntity(API_ENDPOINT, Post[].class);
+//		Post[] resp = httpEntity.getBody();
+//		
+//		List<Post> postsList = new ArrayList<Post>(Arrays.asList(resp));
+//		postsList.add(new Post(100, 10, "Adrian", "Baran"));
+//		
+//		for (Post post : postsList) {
+//			System.out.println(post.getTitle());
+//		}
 		
 		// == TWITTER SECTION ==
 		
